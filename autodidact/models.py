5eb799b3839f224312e70c7299c1a24bcddf9dea
Jaap Joris Vens <jj@rtts.eu>
2015-12-03 00:07:05 +0100

    Created polymorphic inherited models to solve the heterogeneous activities problem

--- autodidact/models.py
+++ autodidact/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 from django.conf import settings
+from polymorphic import PolymorphicModel
 
 class Discipline(models.Model):
-    name = models.CharField(max_length=10, choices=(
-        ('PSY', 'Psychology'),
-        ('SOC', 'Sociology'),
-        ('PEW', 'Human Resource Studies'),
-        ('OW', 'Organization Studies'),
-        ('GMSI', 'Global Management of Social Issues'),
-    ))
-    
+    name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
 
@@ -20,32 +15,85 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
-class Module(models.Model):
-    name = models.CharField(max_length=255)
-    courses = models.ManyToManyField(Course, related_name='modules')
-
-    def __str__(self):
-        return self.name
-
 class Assignment(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(blank=True)
-    datafile = models.FileField(blank=True)
+    result = models.FileField(blank=True)
+    course = models.ManyToManyField(Course, related_name='assignments')
+
+    def __str__(self):
+        return self.description
+
+class Session(models.Model):
+    number = models.IntegerField()
+    courses = models.ManyToManyField(Course, related_name='modules')
+
+    def __str__(self):
+        return 'Session ' + self.name
+
+class Download(models.Model):
+    description = models.CharField(max_length=255)
+    file = models.FileField(blank=True)
+    session = models.ManyToManyField(Session, related_name='downloads')
+
+    def __str__(self):
+        return self.description
+
+class Question(PolymorphicModel):
+    text = models.TextField()
+    type = models.CharField(max_length=10, choices=(
+        ('Bool', 'True/False'),
+        ('Int', 'Integer'),
+        ('Float', 'Floating-point number'),
+        ('String', 'Text input'),
+        ('Radio', 'Select the right answer'),
+        ('Checkbox', 'Select multiple right answers'),
+    ))
+    def __str__(self):
+        return self.description
+
+class BoolQuestion(Question):
+    correct_answer = models.BooleanField();
+
+class IntQuestion(Question):
+    correct_answer = models.IntegerField();
+
+class FloatQuestion(Question):
+    integer_part = models.IntegerField();
+    fractional_part = models.IntegerField();
+    precision = models.IntegerField();
+
+class StringQuestion(Question):
+    correct_answer = models.CharField(max_length=255);
+
+class RadioQuestion(Question):
+    pass
+
+class CheckboxQuestion(Question):
+    pass
+
+class MultipleChoiceAnswer(models.Model):
+    text = models.CharField(max_length=255)
+    correct = models.BooleanField()
+    question = models.ForeignKey(Question)
+
+class Activity(PolymorphicModel):
+    title = models.CharField(max_length=255)
+    session = models.ManyToManyField(Session, related_name='activities')
 
     def __str__(self):
         return self.title
 
-class Student(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+class ReadingActivity(Activity):
+    wall_of_text = models.TextField()
 
-    def __str__(self):
-        return self.user.username
+class QuestionActivity(Activity):
+    question = models.ForeignKey(Question)
 
-class Completed(models.Model):
-    module = models.ForeignKey(Module)
-    student = models.ForeignKey(Student)
+class CompletedActivity(models.Model):
+    activity = models.ForeignKey(Activity)
+    whom = models.ForeignKey(settings.AUTH_USER_MODEL)
     date = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return '%s has completed %s' % (self.student.user.username, self.module.name)
