e9c681c573c7222f9aac146c2ee44275c891e90d
Jaap Joris Vens <jj@rtts.eu>
2015-12-17 15:04:56 +0100

    It works!

--- bps/urls.py
+++ bps/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from autodidact import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.itworks),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
