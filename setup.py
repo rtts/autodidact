6d42071d8f45f431e9eab665652f4a1bfdc4b8f4
Jaap Joris Vens <jj@rtts.eu>
2015-12-17 09:09:01 +0100

    Introduced the "bps" project

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 
 from distutils.core import setup
 
 setup(
-	name = 'autodidact',
-	version = '0.0~0',
-	packages = ['autodidact', 'cas', 'settings'],
+    name = 'autodidact',
+    version = '0.0',
+    url = 'https://github.com/JaapJoris/autodidact',
+    license = 'AGPL',
+    packages = ['autodidact', 'cas', 'bps'],
+    author = 'Jaap Joris Vens',
+    author_email = 'jj@rtts.eu',
+    maintainer = 'Wessel Dankers',
+    maintainer_email = 'wsl@fruit.je',
 )
