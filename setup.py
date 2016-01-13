661daf90c3c3af172fc0277d5549b3e80e173e35
Jaap Joris Vens <jj@rtts.eu>
2016-01-13 17:22:24 +0100

    Recursively include templates in Python package

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
-
+import os
 from distutils.core import setup
+
+def find(dir):
+    package, subdir = os.path.split(dir)
+    matches = []
+    for root, dirnames, filenames in os.walk(dir):
+        root = root[len(package)+1:]
+        for filename in filenames:
+            matches.append(os.path.join(root, filename))
+    return matches
 
 setup(
     name = 'autodidact',
@@ -8,7 +17,7 @@ setup(
     url = 'https://github.com/JaapJoris/autodidact',
     license = 'AGPL',
     packages = ['autodidact', 'bps', 'cas', 'adminsortable'],
-    package_data = {'bps': ['static/*', 'templates/*']},
+    package_data = {'bps': find('bps/static') + find('bps/templates')},
     author = 'Jaap Joris Vens',
     author_email = 'jj@rtts.eu',
     maintainer = 'Wessel Dankers',
