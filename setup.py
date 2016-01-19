f85e65c75198c125393aed8790740d7ee018b7ad
Jaap Joris Vens <jj@rtts.eu>
2016-01-19 15:38:10 +0100

    Automatically find Python modules and package data when installing with setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,45 @@
 import os
 from distutils.core import setup
 
-def find(dir):
-    package, subdir = os.path.split(dir)
+def find_packages(exclude=[]):
+    dirs = find_package_dirs(exclude)
+    return [module_name(dir) for dir in dirs]
+
+def find_package_data(exclude=[]):
+    data = {}
+    for dir in find_package_dirs(exclude):
+        files = find_files(dir)
+        if files:
+            data[module_name(dir)] = files
+    return data
+
+def find_package_dirs(exclude=[]):
+    packages = []
+    for curdir, dirs, files in os.walk('.'):
+        dirs[:] = [d for d in dirs if d not in exclude]
+        if "__init__.py" in files and os.path.dirname(curdir) in packages + ['.']:
+            packages.append(curdir)
+    return packages
+
+def find_files(dir):
     matches = []
-    for root, dirnames, filenames in os.walk(dir):
-        root = root[len(package)+1:]
-        for filename in filenames:
-            matches.append(os.path.join(root, filename))
+    for curdir, dirs, files in os.walk(dir):
+        for file in files:
+            if not file.endswith(('.py', 'pyc', '.gitignore')):
+                subdir = curdir.split(dir)[1].lstrip('/')
+                matches.append(os.path.join(subdir, file))
     return matches
+
+def module_name(dir):
+    return dir.lstrip('/.').replace('/', '.')
 
 setup(
     name = 'autodidact',
     version = '0.1',
     url = 'https://github.com/JaapJoris/autodidact',
     license = 'AGPL',
-    packages = ['autodidact', 'bps', 'cas', 'adminsortable'],
-    package_data = {'bps': find('bps/static') + find('bps/templates')},
+    packages = find_packages(),
+    package_data = find_package_data(),
     author = 'Jaap Joris Vens',
     author_email = 'jj@rtts.eu',
     maintainer = 'Wessel Dankers',
