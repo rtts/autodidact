4880d47dcd462c5ad5a8fec65f98feb480c52b2d
Jaap Joris Vens <jj@rtts.eu>
2016-01-19 11:45:28 +0100

    PYTHONPATH workaround in manage.py and wsgi.py

--- bps/manage.py
+++ bps/manage.py
@@ -2,6 +2,11 @@
 import os
 import sys
 
+# Add the parent directory to the PYTHONPATH
+abspath = os.path.abspath(__file__)
+parent_dir = os.path.dirname(os.path.dirname(abspath))
+sys.path.insert(1,parent_dir)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bps.settings")
 
--- bps/wsgi.py
+++ bps/wsgi.py
@@ -4,8 +4,9 @@ import os, sys
 # WSGIDaemonProcess does NOT set the python path.  To prevent being unable
 # to load the settings, we add the parent directory to the path here:
 
-parent_dir = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(parent_dir)
+abspath = os.path.abspath(__file__)
+parent_dir = os.path.dirname(os.path.dirname(abspath))
+sys.path.insert(1,parent_dir)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bps.settings")
 
