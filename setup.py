673216bc8358484cd1718caff60f9a6c1ee93614
Jaap Joris Vens <jj@rtts.eu>
2016-01-26 16:26:10 +0100

    Commit for release 0.3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def module_name(dir):
 
 setup(
     name = 'autodidact',
-    version = '0.2',
+    version = '0.3',
     url = 'https://github.com/JaapJoris/autodidact',
     license = 'AGPL',
     packages = find_packages(exclude=['tests']),
