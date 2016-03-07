81629ae086ebec4114cce16516f4494d72b30941
Jaap Joris Vens <jj@rtts.eu>
2016-03-08 00:40:18 +0100

    Forget about ajax views for now

--- autodidact/urls.py
+++ autodidact/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from .views import *
-from .ajax_views import *
 
 urlpatterns = [
     url(r'^$', homepage, name='homepage'),
@@ -10,5 +9,4 @@ urlpatterns = [
     url(r'^([^/]+)/session/([0-9]+)/$', session, name='session'),
     url(r'^([^/]+)/session/([0-9]+)/assignment/([0-9]+)/$', assignment, name='assignment'),
     url(r'^([^/]+)/session/([0-9]+)/student/([^/]+)/$', progress, name='progress'),
-    url(r'^([^/]+)/session/([0-9]+)/progresses/$', progresses, name='progresses'),
 ]
