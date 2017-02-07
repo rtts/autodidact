5ab76906d5047954fab5a74d6729a31b472c7a9b
Jaap Joris Vens <jj@rtts.eu>
2017-02-07 17:12:45 +0100

    Fix CSS issues in Internet Explorer

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.argv[-1] == 'test':
 
 setup(
     name = 'autodidact',
-    version = '1.4.2',
+    version = '1.4.3',
     url = 'https://github.com/JaapJoris/autodidact',
     author = 'Jaap Joris Vens',
     author_email = 'jj@rtts.eu',
